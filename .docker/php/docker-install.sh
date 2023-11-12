396a26f780c8e4da9a65454b00b11671a6664c98
JZacharie <joseph@zacharie.org>
2023-11-12 05:49:30 +0100

    Update docker-install.sh

--- .docker/php/docker-install.sh
+++ .docker/php/docker-install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "Install Pimcore"
-vendor/bin/pimcore-install --skip-database-config --ignore-existing-config --no-interaction
+if [ -e "/var/www/html/var/tmp/.pimcore_installed" ]; then
+    echo "Pimcore existe."
+else
+    echo "Install Pimcore"
+    vendor/bin/pimcore-install --skip-database-config --ignore-existing-config --no-interaction
 
-rm -rf var/config/system.yml
-rm -rf var/cache
-
-touch /var/www/html/var/tmp/.pimcore_installed
+    rm -rf var/config/system.yml
+    rm -rf var/cache
+    touch /var/www/html/var/tmp/.pimcore_installed
+fi
